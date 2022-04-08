d5a7a112f68b92250690a2671690f6bbe5498e33
tqphu27 <90370260+tqphu27@users.noreply.github.com>
2022-04-08 11:15:08 +0700

    Update config.py

--- MC_OCR/mc_ocr/config.py
+++ MC_OCR/mc_ocr/config.py
@@ -1,6 +1,8 @@
 import os
 
 CONFIG_ROOT = os.path.dirname(__file__)
+
+#Path output file
 OUTPUT_ROOT = '/content/gdrive/MyDrive/MC_OCR/output'
 
 
@@ -24,6 +26,8 @@ def output_path(sub_path):
     return path
 
 gpu = '0'  # None or 0,1,2...
+
+#file name trong thư mục mc_ocr/data.
 dataset = 'predict_image'
 # mc_ocr_train_filtered
 # mc_ocr_private_test
@@ -33,12 +37,16 @@ raw_train_img_dir = full_path('data/predict_image')
 raw_img_dir=full_path('data/{}'.format(dataset))
 
 #detector
+#Path file txt sau khi detector
 det_out_txt_dir=full_path('/content/gdrive/MyDrive/MC_OCR/output/text_detector/predict_image/txt')
 
-# rotation corrector
+# rotation corrector  
 rot_drop_thresh = [.5, 2]
 rot_visualize = True
+#File weights
 rot_model_path = full_path('rotation_corrector/weights/mobilenetv3-Epoch-487-Loss-0.03-Acc-0.99.pth', file=True)
+
+#output 
 rot_out_img_dir = output_path('rotation_corrector/{}/imgs'.format(dataset))
 rot_out_txt_dir = output_path('rotation_corrector/{}/txt'.format(dataset))
 rot_out_viz_dir = output_path('rotation_corrector/{}/viz_imgs'.format(dataset))
